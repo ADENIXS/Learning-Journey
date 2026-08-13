85c992185b2397feca796132392d6abee23a85c7
ADENIXS_CREATE <denisagendia44@gmail.com>
2026-08-13 10:04:28 -0400

    Add player input movement
    Reducing Movement Speed in the next update.

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,10 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        //Serialized fields for movement values meaning they can be set in the Unity Inspector
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0f; // No vertical movement in this example this is a jump movemnet
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, yValue, zValue);
     }
 }
